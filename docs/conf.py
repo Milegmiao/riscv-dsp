a6deaeffe7f44928a1e5e43def839738f8b757f9
wujunning11@gmail.com <YIZHIaini55>
2021-04-30 17:22:31 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'ReadtheDocsTemplate.tex', u'Haawking RISC-V DSP Documentations',
+   u'Haawking RISC-V DSP', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'readthedocstemplate', u'Haawking RISC-V DSP Documentations',
+     [u'Haawking RISC-V DSP'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'ReadtheDocsTemplate', u'Haawking RISC-V DSP Documentations',
+   u'Haawking RISC-V DSP', 'ReadtheDocsTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
 
